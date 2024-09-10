c87eb5808c5742e655a23663a3e5394da415a861
Helena Utzig <helenautzig@gmail.com>
2024-09-10 11:28:05 +0300

    initial commit header

---
+++ include/pipex.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hutzig <hutzig@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/10 09:49:42 by hutzig            #+#    #+#             */
+/*   Updated: 2024/09/10 10:16:24 by hutzig           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include "../lib/libft/libft.h"	/* libft library */
+# include <unistd.h>			/* fork, pipe, r/w file, close ... */
+# include <string.h>			/* sterror */
+# include <sys/wait.h>			/* waitpid, wait */
+# include <errno.h>			/* errno */
+# include <fcntl.h>			/* open */
+/*
+# define
+
+typedef struct s_name
+{
+
+}
+*/
+#endif
