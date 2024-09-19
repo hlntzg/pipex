2e41b1dd826f65234e92e95a2f4d105c7662bd59
Helena Utzig <helenautzig@gmail.com>
2024-09-19 12:27:27 +0300

    get_path and initialize struct

--- include/pipex.h
+++ include/pipex.h
@@ -6,27 +6,36 @@
 /*   By: hutzig <hutzig@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 09:49:42 by hutzig            #+#    #+#             */
-/*   Updated: 2024/09/10 11:38:53 by hutzig           ###   ########.fr       */
+/*   Updated: 2024/09/19 12:06:24 by hutzig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
 
+# include <stdio.h>			///// PRINTF, REMOVE BEFORE SUBMISSION
+
+
 # include "../lib/libft/libft.h"	/* libft library */
-# include <unistd.h>			/* fork, pipe, r/w file, close ... */
+# include <unistd.h>			/* fork, pipe, r/w file, close, dup2, execve */
 # include <string.h>			/* sterror */
 # include <sys/wait.h>			/* waitpid, wait */
 # include <errno.h>			/* errno */
 # include <fcntl.h>			/* open */
-/*
-# define
 
-typedef struct s_name
+# define STDERR 2
+# define STDOUT 1
+# define STDIN 0
+
+typedef struct s_pipex
 {
+	int	ac;
+	char	**av;
+	char	**envp;
+	char	**path;
+	int	*fd;
+}	t_pipex;
 
-}
-*/
 
 void	log_guide(void);
 void	log_error(char *str);
--- sources/main.c
+++ sources/main.c
@@ -6,22 +6,44 @@
 /*   By: hutzig <hutzig@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 10:17:04 by hutzig            #+#    #+#             */
-/*   Updated: 2024/09/10 11:40:59 by hutzig           ###   ########.fr       */
+/*   Updated: 2024/09/19 12:24:01 by hutzig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int	main(int argc, char **argv, char **envp) // envp ??
+void	get_path(t_pipex *data, char **envp)
 {
-	(void)argv;
-	(void)envp;
+	if (!(*envp))
+		return ;
+	while (*envp != NULL && ft_strncmp(*envp, "PATH=", 5))
+		envp++;
+	data->path = ft_split((*envp) + 5, ':');
+	if (!data->path)
+		return ; // check malloc fails, any other error?
+}
+
+void	init_pipex_data(int argc, char **argv, char **envp, t_pipex *data)
+{
+	data->ac = argc;
+	data->av = argv;
+	data->envp = envp;
+	data->path = NULL;
+	data->fd = NULL;
+	get_path(data, envp);
+}
+
+int	main(int argc, char **argv, char **envp) // # envp: the environmental variable
+{
+	t_pipex	data;
+
 	if (argc != 5)
 	{
 		log_error("Error: Incorrect number of arguments");
 		log_guide();
 		return (EXIT_FAILURE);
 	}
-	ft_putstr_fd("working pipex program", 1);
+	init_pipex_data(argc, argv, envp, &data);
+	//ft_putstr_fd("working pipex program", 1);
 	return (EXIT_SUCCESS);
 }
